107d75aaaecd3a2f1fdba912e540f064a43f49aa
Daniel <rasay_d@yahoo.com>
2025-03-20 21:56:16 +0800

    Complete initial logic on 16x16 flexgrid via %

--- file.js
+++ file.js
@@ -1,9 +1,9 @@
 //TODO: continue LOGIC OF STEP 2 pseudocode; 3/20/2025
 // Logic #1 - commit 3/20/25
 function makeGrid() {
-    for (let i=0;i<=256;++i) {
+    for (let i=0;i<256;++i) {
         let divSet = document.createElement("div")
-        divSet.style.cssText = "border: 1px solid black; flex-grow: 1; flex-shrink: 0;"
+        divSet.style.cssText = "box-sizing: border-box; border: 1px solid black; flex-grow: 1; flex-shrink: 0; flex-basis: 6.25%"
         let mainCont = document.querySelector(".main-container");
         mainCont.appendChild(divSet);
     };
@@ -11,3 +11,4 @@ function makeGrid() {
 
 makeGrid();
 
+//CONTINUE TOMORROW 3/20/2025
