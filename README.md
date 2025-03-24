07a72d50f6498b32b8695469469edde26dda74b0
Daniel <rasay_d@yahoo.com>
2025-03-24 14:47:42 +0800

    Create a clear grid function, mini tweak on README.md

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Etch_A_Sketch PSEUDOCODE:
 
-- Create #main-container w/ Etch_A_Sketch header (Flexbox)
+1) Create #main-container w/ Etch_A_Sketch header (Flexbox)
 
 2) Make a Javascript function that produces 16x16 grid (default grid, own preferred size) on #main-container
 
-- TODO: Add an event listener on created boxes that changes Grid box color on hover (color: blue)
+3) Add an event listener on created boxes that changes Grid box color on hover (color: blue)
 
-- Create a button that asks for a prompt to change Grid box dimension
+- TODO: Create a button that asks for a prompt to change Grid box dimension, and then change the Grid box to the selected dimension (max 100 per side)
 
 - Create a button and a function that randomizes the hover color
 - Change color progression on hover to darker opacity by 10% each hover
--- file.js
+++ file.js
@@ -1,8 +1,13 @@
+ // Logic Commit 3/24/25
+function clearGrid() {
+    document.querySelector(".main-container").innerHTML = "";
+}
+
  function makeGrid() {
     for (let i=0;i<256;++i) {
         let divSet = document.createElement("div")
-        divSet.style.cssText = "box-sizing: border-box; border: 1px solid black; flex-grow: 1; flex-shrink: 0; flex-basis: 6.25%"
-        // Logic Commit 3/22/25
+
+        divSet.style.cssText = `box-sizing: border-box; border: 1px solid black; flex-grow: 1; flex-shrink: 0; flex-basis: ${100/16}%`
         divSet.classList.add("hover-color");
         
         let mainCont = document.querySelector(".main-container");
@@ -11,8 +16,8 @@
 }
 
 makeGrid();
+clearGrid()
 
- // Logic Commit 3/22/25
 document.querySelectorAll(".hover-color").forEach(
     function(node){
         node.addEventListener("mouseover", colorBox);
@@ -23,4 +28,6 @@ function colorBox(evt) {
     evt.currentTarget.style.backgroundColor = "blue";
     // alert(`${evt}`);
 }
+
+
 
